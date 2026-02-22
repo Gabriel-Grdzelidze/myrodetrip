8a949fdd00f2201914c479d2292841b65ece5c07
grdzelo <grdzelidzegabriel@gmail.com>
2026-02-22 19:37:34 +0400

    2

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -1,5 +1,6 @@
 import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { NextRequest } from 'next/server';
 
 import { typeDefs } from '../../../graphql/typedefs'; 
@@ -8,14 +9,11 @@ import { resolvers } from '../../../graphql/resolvers';
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  plugins: [ApolloServerPluginLandingPageLocalDefault()],
+  plugins: [
+    ApolloServerPluginLandingPageLocalDefault(),
+  ],
 });
 
 const handler = startServerAndCreateNextHandler<NextRequest>(server);
 
 export { handler as GET, handler as POST };
-
-function ApolloServerPluginLandingPageLocalDefault(): import("@apollo/server").ApolloServerPlugin<import("@apollo/server").BaseContext> {
-    throw new Error('Function not implemented.');
-}
-
--- graphql/resolvers.tsx
+++ graphql/resolvers.tsx
@@ -1,6 +1,4 @@
-import { PrismaClient } from "../generated/prisma";
-
-const prisma = new PrismaClient();
+import { prisma } from "../prisma/db"
 
 export const resolvers = {
   Query: {
